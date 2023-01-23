cc44d18b29d41f14b497ea72fcd2cea0242d5c17
pmirzapour <pmirzapour@bol.com>
2023-01-23 12:05:58 +0100

    remove unused import

--- src/main/java/com/abn/recipe/api/controller/IngredientController.java
+++ src/main/java/com/abn/recipe/api/controller/IngredientController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/ingredients")
