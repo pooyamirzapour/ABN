8ec21da7a544bb26a6c0146b3e18251932944366
pmirzapour <pmirzapour@bol.com>
2023-01-23 12:06:49 +0100

    remove unused import

--- src/main/java/com/abn/recipe/api/controller/RecipeController.java
+++ src/main/java/com/abn/recipe/api/controller/RecipeController.java
@@ -13,7 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 
